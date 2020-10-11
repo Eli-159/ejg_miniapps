586f9cca359bd5fed09905347123efc3a4e1e7c5
Eli <64872006+Eli-159@users.noreply.github.com>
2020-10-12 07:29:50 +1000

    Update instructions.js

--- snppt/js/instructions.js
+++ snppt/js/instructions.js
@@ -1,5 +1,5 @@
 document.addEventListener('DOMContentLoaded', () => {
-    fetch('https://snppt-app-5546u.ondigitalocean.app/bookmarklet/SN_Bookmarklet.txt')
+    fetch('https://ejg-miniapps-svb26.ondigitalocean.app/snppt/bookmarklet/SN_Bookmarklet.txt')
     .then(res => res.text())
     .then(js => {document.getElementById('bookmarkletCode').textContent = js});
 });
