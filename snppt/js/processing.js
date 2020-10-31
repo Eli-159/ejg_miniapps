826b07ca1a2408993c87957ce3b62fa4b54386da
Eli <64872006+Eli-159@users.noreply.github.com>
2020-11-01 07:36:22 +1000

    Update processing.js

--- snppt/js/processing.js
+++ snppt/js/processing.js
@@ -222,7 +222,7 @@ document.addEventListener('DOMContentLoaded', () => {
         for (let i = 0; i < processedData.length; i++) {
             let slide = powerpointFunctions.createSlide(pres, includeLogo.checked, colours);
             powerpointFunctions.createTextBox(slide, processedData[i]['subject'], {
-                fontSize: 40,
+                //fontSize: 40,
                 x: 1,
                 y: 0.3,
                 align: 'center',
