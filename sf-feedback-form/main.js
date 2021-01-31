35a73bead8e7e1241a76823c822e05063dddc7a9
Eli <64872006+Eli-159@users.noreply.github.com>
2021-01-31 14:15:29 +1000

    Google Sheets API Experimenting

---
+++ sf-feedback-form/main.js
@@ -0,0 +1,9 @@
+document.addEventListener('DOMContentLoaded', () => {
+    document.getElementById('hiddenFormResponse').addEventListener('load', () => {
+        alert('Form Submitted!');
+    });
+});
+
+// https://drive.google.com/file/d/1NppR45J1SWUfG_ZBWszIK57GyO06n8V9/view?usp=sharing
+// https://docs.google.com/uc?export=download&id=YourIndividualID
+// https://docs.google.com/uc?export=download&id=1NppR45J1SWUfG_ZBWszIK57GyO06n8V9
