137ea38d5a6084f39a3358a55c002408def7862f
Eli <64872006+Eli-159@users.noreply.github.com>
2021-02-16 08:17:51 +1000

    Fetch Bookmarklet Script Update
    Doesn't fail when used more than once without a page reload.

--- snppt/bookmarklet/fetch-bookmarklet.js
+++ snppt/bookmarklet/fetch-bookmarklet.js
@@ -1,12 +1,14 @@
-const scriptPromise = new Promise((resolve, reject) => {
-    const script = document.createElement('script');
-    document.body.appendChild(script);
-    script.onload = resolve;
-    script.onerror = reject;
-    script.async = true;
-    script.src = 'https://ejg-miniapps-svb26.ondigitalocean.app/snppt/bookmarklet/SN_Bookmarklet.js';
-});
-  
-scriptPromise.then(() => {
+try {
     copyNoticesDataFunc();
-});
+} catch {
+    new Promise((resolve, reject) => {
+        const script = document.createElement('script');
+        document.body.appendChild(script);
+        script.onload = resolve;
+        script.onerror = reject;
+        script.async = true;
+        script.src = 'https://ejg-miniapps-svb26.ondigitalocean.app/snppt/bookmarklet/SN_Bookmarklet.js';
+    }).then(() => {
+        copyNoticesDataFunc();
+    });
+}
