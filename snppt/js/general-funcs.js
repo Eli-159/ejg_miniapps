14bd4d45f1c6b1521517ad83baa0a816334e6eea
Eli <64872006+Eli-159@users.noreply.github.com>
2021-02-20 16:13:26 +1000

    No Transitions on Page Load

--- snppt/js/general-funcs.js
+++ snppt/js/general-funcs.js
@@ -32,3 +32,15 @@ function getStringDate(format, inputDate) {
         return year + month + day;
     }
 }
+
+// Waits for the document to load.
+document.addEventListener('DOMContentLoaded', () => {
+    // Waits for 50ms.
+    setTimeout(() => {
+        // Gets all elements with the 'stop-page-load-transition' class and removes it, meaning the transitions can be applied.
+        const noTransClass = document.getElementsByClassName('stop-page-load-transition');
+        for (let i = 0; i < noTransClass.length; i++) {
+            noTransClass[i].classList.remove('stop-page-load-transition');
+        };
+    }, 50);
+});
