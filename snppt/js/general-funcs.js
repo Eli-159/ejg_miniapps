f62f603e59d631d986faf0b92d8fad8a1b06817a
Eli <64872006+Eli-159@users.noreply.github.com>
2021-02-21 06:33:49 +1000

    No Transition on Page Load Fix

--- snppt/js/general-funcs.js
+++ snppt/js/general-funcs.js
@@ -39,8 +39,8 @@ document.addEventListener('DOMContentLoaded', () => {
     setTimeout(() => {
         // Gets all elements with the 'stop-page-load-transition' class and removes it, meaning the transitions can be applied.
         const noTransClass = document.getElementsByClassName('stop-page-load-transition');
-        for (let i = 0; i < noTransClass.length; i++) {
-            noTransClass[i].classList.remove('stop-page-load-transition');
+        while (noTransClass.length > 0) {
+            noTransClass[0].classList.remove('stop-page-load-transition');
         };
     }, 50);
 });
